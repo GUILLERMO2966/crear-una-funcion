f1cafa3dc016d5b1c7d3e1884d98d7f0042da31b
GUILLERMO2966 <71560170+GUILLERMO2966@users.noreply.github.com>
2020-11-12 14:11:24 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let T1 = 0
 let T2 = 0
 let T3 = 0
+let TEMPERATURA = 0
 function TEMPERATURA_PROMEDIO () {
     T1 = input.temperature()
     basic.pause(5000)
@@ -9,3 +10,13 @@ function TEMPERATURA_PROMEDIO () {
     T3 = input.temperature()
     return T1 + (T2 + T3 / 3)
 }
+basic.forever(function () {
+    TEMPERATURA = TEMPERATURA_PROMEDIO()
+    if (TEMPERATURA > 25) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+    basic.pause(5000)
+    basic.showNumber(TEMPERATURA)
+})
